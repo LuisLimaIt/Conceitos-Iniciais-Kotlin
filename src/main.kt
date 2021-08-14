d104cb2c5c1bde7a213224b0d9a9815dadb1b2ad
LuisLimaIt <pls.luis@gmail.com>
2021-08-13 23:41:45 -0300

    Criação de funções

--- src/main.kt
+++ src/main.kt
@@ -19,7 +19,24 @@ Lembrando sobre unidades de medida:
 1 gigabyte (GB) = 1024 megabytes
  */
 
+fun calculaBonus(a: Int, b: Int, c: Int) {
+    println("O bônus é: ${a + b * c * 2}")
+}
+
+fun hello(nome: String) : String {
+    return "Olá, $nome"
+}
+
 fun main() {
+
+    val a = 10
+    val b = 20
+    val c = 30
+
+    calculaBonus(a, b, c)
+
+    println(hello("Luis"))
+
     println("Olá mundo!")
 
     //Variáveis mutáveis
